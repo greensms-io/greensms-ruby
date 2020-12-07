2168081b66ac4e88933ccd30110d1f3889dfc4d1
Manan Jadhav <manan.jadhav@gmail.com>
2020-12-08 03:12:29 +0530

    Camelize keys for response

--- lib/greensms/utils/str.rb
+++ lib/greensms/utils/str.rb
@@ -8,4 +8,21 @@ module GreenSMS
       return false
     end
   end
+
+
+
+  def GreenSMS.camelize(value)
+    case value
+      when Array
+        value.map { |v| GreenSMS.camelize(v) }
+      when Hash
+        Hash[value.map { |k, v| [GreenSMS.camelize(k), GreenSMS.camelize(v)] }]
+      when String
+        value.split('_').inject{|m, p| m + p.capitalize}
+      else
+        value
+      end
+  end
 end
+
+
