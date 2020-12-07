df9357e5f270fbda28ede799f309d1fab693aca0
Manan Jadhav <manan.jadhav@gmail.com>
2020-12-08 03:11:31 +0530

    Rest Client with response complete

--- lib/greensms/http/rest.rb
+++ lib/greensms/http/rest.rb
@@ -1,6 +1,8 @@
+require "uri"
 require "faraday"
-require "greensms/utils/str"
 require "json"
+require "greensms/utils/str"
+require "greensms/http/error"
 
 
 module GreenSMS
@@ -10,6 +12,7 @@ module GreenSMS
         @token = nil
         @default_data = {}
         @default_params = {}
+        @use_camel_case = false
 
         opts.each { |name, value| instance_variable_set("@#{name}", value) }
       end
@@ -30,18 +33,21 @@ module GreenSMS
         kwargs.delete('url')
         kwargs.delete('method')
 
-        headers = {}
+        headers = { }
         if kwargs.has_key? 'headers' then
-          headers = kwargs['headers']
+          kwargs['headers'].each { |name, value| headers[name] = value }
         end
 
         if ! GreenSMS.is_null_or_empty(@token) then
           headers['Authorization'] = 'Bearer ' + @token
         end
 
+
         params = {}
 
         @connection = Faraday.new(url: url) do |f|
+          f.request :multipart
+          f.request :url_encoded
           f.headers = headers
           f.adapter Faraday.default_adapter
         end
@@ -54,9 +60,30 @@ module GreenSMS
           kwargs['params'].each { |name, value| params[name] = value }
         end
 
-        resp = @connection.send(method.downcase.to_sym, url, params)
-        puts resp.body
+        d = {}
+        if method == 'POST' then
+          d = d.to_json
+        end
 
+        url = url + "?" + hash_to_query(params)
+        puts url
+
+        resp = @connection.send(method.downcase.to_sym, url, d)
+        resp = JSON.parse resp.body
+        if resp.has_key? 'error' then
+          raise RestError.new(resp['error'], resp['code'])
+        end
+
+        if @use_camel_case then
+          resp = GreenSMS.camelize(resp)
+        end
+
+        return resp
+      end
+
+      private
+      def hash_to_query(hash)
+        return URI.encode(hash.map{|k,v| "#{k}=#{v}"}.join("&"))
       end
 
     end
