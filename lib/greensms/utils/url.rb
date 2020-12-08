88e1cf2e8031633f5cfffcefbdfa2178cfaf1fce
Manan Jadhav <manan.jadhav@gmail.com>
2020-12-08 05:42:56 +0530

    Added util to build url

--- lib/greensms/utils/url.rb
+++ lib/greensms/utils/url.rb
@@ -1,7 +1,18 @@
 require "greensms/constants"
+require "greensms/utils/str"
 
 module GreenSMS
   def GreenSMS.base_url()
     return GreenSMS::BASE_URL
+  end
+
+  def GreenSMS.build_url(base_url, url_parts)
+    if GreenSMS.is_null_or_empty(base_url) then
+      raise StandardError.new "Base URL cannot be empty"
+    end
+
+    url = base_url + "/" + url_parts.join("/")
+    return url
+
   end
 end
