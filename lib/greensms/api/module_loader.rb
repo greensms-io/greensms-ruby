de2234e962a175a0dcae6e9acaf3f3e0cb3e4b47
Manan Jadhav <manan.jadhav@gmail.com>
2020-12-08 05:42:00 +0530

    Completed dynamic module loader with functions

--- lib/greensms/api/module_loader.rb
+++ lib/greensms/api/module_loader.rb
@@ -1,4 +1,6 @@
 require "greensms/api/modules"
+require "greensms/api/module"
+require "greensms/utils/url"
 
 module GreenSMS
   module API
@@ -31,14 +33,56 @@ module GreenSMS
           next if is_static_module
 
           module_versions.each do | version, version_functions |
+
             if !@module_map[module_name].has_key?(version) then
               @module_map[module_name][version] = {}
             end
 
-          end
+            version_functions.each do |function_name, definition|
+              if !@module_map[module_name][version].has_key?(function_name) then
+                @module_map[module_name][version][function_name] = {}
+              end
 
+              module_schema = nil
+              schema_exists = module_info.has_key? 'schema' && module_info['schema'].has_key?(version) && module_info['schema'][version].has_key?(function_name)
+              if schema_exists == true then
+                module_schema = module_info['schema'][version][function_name]
+              end
+
+              url_args = []
+              if !module_info.has_key? 'static' || module_info['sgtatic'] != true then
+                url_args.append(module_name)
+              end
+              url_args.append(function_name)
+
+              api_url = GreenSMS.build_url(shared_options['base_url'], url_args)
+              @module_map[module_name][version][function_name] = module_api(
+                        shared_options:shared_options, api_url:api_url, definition:definition, module_schema:module_schema)
+
+            end
+          end
         end
+
+        puts @module_map
+
         return @module_map
+
+      end
+
+      private
+      def module_api(kwargs)
+
+        rest_client = kwargs[:shared_options][:rest_client]
+        module_schema = kwargs['module_schema']
+
+        request_params = {
+            'url' =>  kwargs['api_url'],
+            'method' => kwargs[:definition][:method],
+        }
+
+        mod = GreenSMS::API::Module.new(rest_client, module_schema, request_params)
+        api_call = mod.method(:api_func)
+        return api_call
 
       end
     end
