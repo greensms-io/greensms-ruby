987d846ce17889b2aec37c83e7c0e512889e7213
Manan Jadhav <manan.jadhav@gmail.com>
2020-12-09 00:05:16 +0530

    Lint fixes

--- lib/greensms/utils/validator.rb
+++ lib/greensms/utils/validator.rb
@@ -1,11 +1,10 @@
 require "json-schema"
 require "greensms/http/error"
 
-
 module GreenSMS
   def GreenSMS.validate(schema, data)
     res = JSON::Validator.fully_validate(schema, data, :errors_as_objects => true)
-    if ! res.nil? && res.length > 0
+    if !res.nil? && res.length > 0
       raise RestError.new(msg: "Validation Error", code: 1, params: res)
     end
     return nil
