14b7516eac58fa34ffbd972296dc86adda4d3cf4
Manan Jadhav <manan.jadhav@gmail.com>
2020-12-13 14:45:17 +0530

    Added example for hlr

---
+++ examples/hlr.rb
@@ -0,0 +1,11 @@
+require_relative "init"
+
+@client = Example::CLIENT
+
+puts "HLR Send"
+resp = @client.hlr.send(to: "79260000121", txt: '1221')
+puts resp
+
+sleep(3)
+puts "HLR Status"
+puts @client.hlr.status(id: resp["request_id"], to: '79260000121')
