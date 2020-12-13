3398bd03afe112fb34184d5e4f465b85c2c37e26
Manan Jadhav <manan.jadhav@gmail.com>
2020-12-13 16:55:57 +0530

    Added all unit tests

--- test/pay_test.rb
+++ test/pay_test.rb
@@ -1,13 +1,13 @@
 require "test_helper"
 
-class CallTest < Minitest::Test
+class PayTest < Minitest::Test
   i_suck_and_my_tests_are_order_dependent!
   @@resp = nil
 
   def test_send
     @client = Utility::CLIENT
     phone_number = Utility::random_phone_number
-    resp = @client.call.send(to: phone_number)
+    resp = @client.pay.send(to: phone_number, amount: 10, tag: "RubyTest")
     assert_equal true, resp.has_key?("request_id")
     @@resp = resp
   end
@@ -15,15 +15,14 @@ class CallTest < Minitest::Test
   def test_status
     @client = Utility::CLIENT
     sleep(3)
-    resp = @client.call.status(id: @@resp["request_id"])
+    resp = @client.pay.status(id: @@resp["request_id"], extended: true)
     assert_equal true, resp.has_key?("status")
   end
 
   def test_validation_error
-    sleep(10) ## Ensure this assert runs last
     error = assert_raises(StandardError) do
       @client = Utility::CLIENT
-      @client.call.send
+      @client.pay.send
     end
     assert_equal "Validation Error", error.message
   end
--- test/voice_test.rb
+++ test/voice_test.rb
@@ -1,13 +1,13 @@
 require "test_helper"
 
-class CallTest < Minitest::Test
+class VoiceTest < Minitest::Test
   i_suck_and_my_tests_are_order_dependent!
   @@resp = nil
 
   def test_send
     @client = Utility::CLIENT
     phone_number = Utility::random_phone_number
-    resp = @client.call.send(to: phone_number)
+    resp = @client.voice.send(to: phone_number, txt: "1111", lang: "en")
     assert_equal true, resp.has_key?("request_id")
     @@resp = resp
   end
@@ -15,15 +15,14 @@ class CallTest < Minitest::Test
   def test_status
     @client = Utility::CLIENT
     sleep(3)
-    resp = @client.call.status(id: @@resp["request_id"])
+    resp = @client.voice.status(id: @@resp["request_id"], extended: true)
     assert_equal true, resp.has_key?("status")
   end
 
   def test_validation_error
-    sleep(10) ## Ensure this assert runs last
     error = assert_raises(StandardError) do
       @client = Utility::CLIENT
-      @client.call.send
+      @client.voice.send
     end
     assert_equal "Validation Error", error.message
   end
