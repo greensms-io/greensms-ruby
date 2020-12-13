c743b63d12f260ba76119f46f440976ac3f0f6af
Manan Jadhav <manan.jadhav@gmail.com>
2020-12-13 17:10:38 +0530

    Done with General Test

--- test/general_test.rb
+++ test/general_test.rb
@@ -1,1 +1,19 @@
+require "test_helper"
 
+class GeneralTest < Minitest::Test
+  def test_lookup
+    @client = Utility::CLIENT
+    resp = @client.whois.lookup(to: "79260000000")
+    puts resp
+    has_def = resp.has_key?("def")
+    has_region = resp.has_key?("region")
+    assert_equal true, has_def && has_region
+  end
+
+  def test_status
+    @client = Utility::CLIENT
+    sleep(3)
+    resp = @client.status
+    assert_equal true, resp.has_key?("status")
+  end
+end
