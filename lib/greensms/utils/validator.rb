231ca9748336331701c893010fcff29bb5e146f0
Manan Jadhav <manan.jadhav@gmail.com>
2020-12-13 16:26:52 +0530

    Corrected validation error call

--- lib/greensms/utils/validator.rb
+++ lib/greensms/utils/validator.rb
@@ -5,7 +5,7 @@ module GreenSMS
   def GreenSMS.validate(schema, data)
     res = JSON::Validator.fully_validate(schema, data, :errors_as_objects => true)
     if !res.nil? && res.length > 0
-      raise RestError.new(msg: "Validation Error", code: 1, params: res)
+      raise RestError.new("Validation Error", 1, res)
     end
     return nil
   end
